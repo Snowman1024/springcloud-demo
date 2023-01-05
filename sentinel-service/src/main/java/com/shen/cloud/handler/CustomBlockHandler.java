22e8ff7d44e2af35ee3cd1abf21e1fbf0117623b
guoqf <guoqf@neusoft.com>
2023-01-05 13:45:33 +0800

    增加sentinel相关

---
+++ sentinel-service/src/main/java/com/shen/cloud/handler/CustomBlockHandler.java
@@ -0,0 +1,12 @@
+package com.shen.cloud.handler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.shen.cloud.domain.CommonResult;
+
+public class CustomBlockHandler {
+
+
+    public CommonResult handleException(BlockException exception){
+        return new CommonResult("自定义限流信息",200);
+    }
+}
